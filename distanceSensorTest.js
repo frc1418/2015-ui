489f0403b8405654c7efedbd6242542ce7953628
lleontan <lleontan@gmail.com>
2015-01-22 15:59:31 -0500

    sensor test now works with network tables

--- distanceSensorTest.js
+++ distanceSensorTest.js
@@ -3,53 +3,18 @@ jQuery(function($){
        if (!("WebSocket" in window)) {
        alert("Your browser does not support web sockets");
        }else{
+	   
        setup();
        }
        function setup(){
+	   console.log("setup Called");
        var host = "ws://localhost:8888/";
        var socket = new WebSocket(host);
-       var $key=$("#keyField");
-       var $txt = $("#textField");
-       var $submitForm = $("#submitForm");
-              $submitForm.submit(function(event){
-                event.preventDefault();
-                var key=$key.val();
-                var text = $txt.val();
-                var protoJson={
-                                 "key":key,
-                                 "value":text,
-                                 "action":"write",
-                                 "sendTo":"#messageText3"
-                                 };
-                var sendFile=JSON.stringify(protoJson);
-                   if(text == ""){
-                   return;
-                   }
-                keyExceptions[key]="#messageText3";
-                   socket.send(sendFile);
-                   //$txt.val("");
-                   });
-       var $readForm=$("#readForm");
-       $readForm.submit(
-                        function(event){
-            event.preventDefault();
-                        var key=$("#readKeyField").val();
-                        var protoJson={
-                        "key":key,
-                        "action":"read",
-                        "sendTo":"#readTextField"
-                        };
-                        var sendFile=JSON.stringify(protoJson);
-                        socket.send(sendFile);
-            });
-
+	   
        if(socket){
        socket.onopen = function(){
        }
        socket.onmessage = function(msg){
-       //checks for sendTo, if no send to id of key
-       //it should in the future find the thing with the id ==key
-       //console.log(msg.data);
        var data=JSON.parse(msg.data);
        console.log(data);
        var value=data['value'];
@@ -57,13 +22,16 @@ jQuery(function($){
        var sendTo=data['sendTo'];
        var event=data['event'];
        console.log("key-"+key+" is changed to "+value);
-       if(event=='valChanged'){
-       sendTo=keyExceptions[key];
-       $(sendTo).text(value);
-       }else if(event=="read"){
-       $(sendTo).val(value);
+	   if(key=='largeSensorValue'|key=="#largeSensorValue"){
+	   h1=value;
+	   }
+	   else if(key=="smallSensorValue"|key=='#smallSensorValue'){
+	   h2=value;
        }
-       
+	   var theLine=d3.select("#theLine")
+	   theLine
+	   .attr("x1","0").attr("y1",svgHeight-h1).attr("x2",linelength)
+	   .attr("y2",svgHeight-h2).attr("stroke","green");
        }
        socket.onclose = function(){
        console.log("socket Closed");}
