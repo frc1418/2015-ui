efec3e3a96a66a07bb17fcc785f3984453cfc9ae
Dustin Spicuzza <dustin@virtualroadside.com>
2015-01-24 14:24:32 -0500

    Format code

--- distanceSensorTest.js
+++ distanceSensorTest.js
@@ -1,43 +1,46 @@
-var keyExceptions=new Array();
+var keyExceptions = new Array();
 
-jQuery(function($){
-       if (!("WebSocket" in window)) {
-       alert("Your browser does not support web sockets");
-       }else{
-	   
-       setup();
-       }
-       function setup(){
-	   console.log("setup Called");
-       var host = "ws://localhost:8888/ws";
-       var socket = new WebSocket(host);
-	   
-       if(socket){
-       socket.onopen = function(){
-       }
-       socket.onmessage = function(msg){
-       var data=JSON.parse(msg.data);
-       console.log(data);
-       var value=data['value'];
-       var key=data['key'];
-       var sendTo=data['sendTo'];
-       var event=data['event'];
-       console.log("key-"+key+" is changed to "+value);
-	   if(key=='largeSensorValue'|key=="#largeSensorValue"){
-	   h1=value;
-	   }
-	   else if(key=="smallSensorValue"|key=='#smallSensorValue'){
-	   h2=value;
-       }
-	   var theLine=d3.select("#theLine")
-	   theLine
-	   .attr("x1","0").attr("y1",svgHeight-h1).attr("x2",linelength)
-	   .attr("y2",svgHeight-h2).attr("stroke","green");
-	   findAngles();
-       }
-       socket.onclose = function(){
-       console.log("socket Closed");}
-       
-       }else{
-       console.log("invalid socket");
-       }}});
+jQuery(function($) {
+	if (!("WebSocket" in window)) {
+		alert("Your browser does not support web sockets");
+	} else {
+
+		setup();
+	}
+	function setup() {
+		console.log("setup Called");
+		var host = "ws://localhost:8888/ws";
+		var socket = new WebSocket(host);
+
+		if (socket) {
+			socket.onopen = function() {
+			}
+			socket.onmessage = function(msg) {
+				var data = JSON.parse(msg.data);
+				console.log(data);
+				var value = data['value'];
+				var key = data['key'];
+				var sendTo = data['sendTo'];
+				var event = data['event'];
+				console.log("key-" + key + " is changed to " + value);
+				if (key == 'largeSensorValue' | key == "#largeSensorValue") {
+					h1 = value;
+				} else if (key == "smallSensorValue"
+						| key == '#smallSensorValue') {
+					h2 = value;
+				}
+				var theLine = d3.select("#theLine")
+				theLine.attr("x1", "0").attr("y1", svgHeight - h1).attr("x2",
+						linelength).attr("y2", svgHeight - h2).attr("stroke",
+						"green");
+				findAngles();
+			}
+			socket.onclose = function() {
+				console.log("socket Closed");
+			}
+
+		} else {
+			console.log("invalid socket");
+		}
+	}
+});
--- testingPage.js
+++ testingPage.js
@@ -1,73 +1,75 @@
-var keyExceptions=new Array();
-jQuery(function($){
-       if (!("WebSocket" in window)) {
-       alert("Your browser does not support web sockets");
-       }else{
-       setup();
-       }
-       function setup(){
-       var host = "ws://localhost:8888/ws";
-       var socket = new WebSocket(host);
-       var $key=$("#keyField");
-       var $txt = $("#textField");
-       var $submitForm = $("#submitForm");
-              $submitForm.submit(function(event){
-                event.preventDefault();
-                var key=$key.val();
-                var text = $txt.val();
-                var protoJson={
-                                 "key":key,
-                                 "value":text,
-                                 "action":"write",
-                                 "sendTo":"#messageText3"
-                                 };
-                var sendFile=JSON.stringify(protoJson);
-                   if(text == ""){
-                   return;
-                   }
-                keyExceptions[key]="#messageText3";
-                   socket.send(sendFile);
-                   //$txt.val("");
-                   });
-       var $readForm=$("#readForm");
-       $readForm.submit(
-                        function(event){
-            event.preventDefault();
-                        var key=$("#readKeyField").val();
-                        var protoJson={
-                        "key":key,
-                        "action":"read",
-                        "sendTo":"#readTextField"
-                        };
-                        var sendFile=JSON.stringify(protoJson);
-                        socket.send(sendFile);
-            });
+var keyExceptions = new Array();
+jQuery(function($) {
+	if (!("WebSocket" in window)) {
+		alert("Your browser does not support web sockets");
+	} else {
+		setup();
+	}
+	function setup() {
+		var host = "ws://localhost:8888/ws";
+		var socket = new WebSocket(host);
+		var $key = $("#keyField");
+		var $txt = $("#textField");
+		var $submitForm = $("#submitForm");
+		$submitForm.submit(function(event) {
+			event.preventDefault();
+			var key = $key.val();
+			var text = $txt.val();
+			var protoJson = {
+				"key" : key,
+				"value" : text,
+				"action" : "write",
+				"sendTo" : "#messageText3"
+			};
+			var sendFile = JSON.stringify(protoJson);
+			if (text == "") {
+				return;
+			}
+			keyExceptions[key] = "#messageText3";
+			socket.send(sendFile);
+			// $txt.val("");
+		});
+		var $readForm = $("#readForm");
+		$readForm.submit(function(event) {
+			event.preventDefault();
+			var key = $("#readKeyField").val();
+			var protoJson = {
+				"key" : key,
+				"action" : "read",
+				"sendTo" : "#readTextField"
+			};
+			var sendFile = JSON.stringify(protoJson);
+			socket.send(sendFile);
+		});
 
-       if(socket){
-       socket.onopen = function(){
-       }
-       socket.onmessage = function(msg){
-       //checks for sendTo, if no send to id of key
-       //it should in the future find the thing with the id ==key
-       //console.log(msg.data);
-       var data=JSON.parse(msg.data);
-       console.log(data);
-       var value=data['value'];
-       var key=data['key'];
-       var sendTo=data['sendTo'];
-       var event=data['event'];
-       console.log("key-"+key+" is changed to "+value);
-       if(event=='valChanged'){
-       sendTo=keyExceptions[key];
-       $(sendTo).text(value);
-       }else if(event=="read"){
-       $(sendTo).val(value);
-       }
-       
-       }
-       socket.onclose = function(){
-       console.log("socket Closed");}
-       
-       }else{
-       console.log("invalid socket");
-       }}});
+		if (socket) {
+			socket.onopen = function() {
+			}
+			socket.onmessage = function(msg) {
+				// checks for sendTo, if no send to id of key
+				// it should in the future find the thing with the id ==key
+				// console.log(msg.data);
+				var data = JSON.parse(msg.data);
+				console.log(data);
+				var value = data['value'];
+				var key = data['key'];
+				var sendTo = data['sendTo'];
+				var event = data['event'];
+				console.log("key-" + key + " is changed to " + value);
+				if (event == 'valChanged') {
+					sendTo = keyExceptions[key];
+					$(sendTo).text(value);
+				} else if (event == "read") {
+					$(sendTo).val(value);
+				}
+
+			}
+			socket.onclose = function() {
+				console.log("socket Closed");
+			}
+
+		} else {
+			console.log("invalid socket");
+		}
+	}
+});
