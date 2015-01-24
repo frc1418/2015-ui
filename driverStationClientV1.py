290cce1cb72f4de8efc7ab0837ca84a4782f4e20
jiggam <jacobmhance>
2015-01-24 13:15:46 -0500

    spacing

--- driverStationClientV1.py
+++ driverStationClientV1.py
@@ -10,7 +10,14 @@ from tornado.options import define, options, parse_command_line
 import logging
 import json
 import os.path
+
+
+
+
 logging.basicConfig(level=logging.DEBUG)
+
+
+
 define("port", default=8888, help="run on the given port", type=int)
 '''
 this should read and set values on a networktable when instructed,
@@ -18,21 +25,25 @@ this should read and set values on a networktable when instructed,
 #link - http://localhost:8889/
 #ip is probably 127.0.0.1
 ipadd='10.14.18.2'
-#if len(ipadd) != 2:
-        #print("Error: specify an IP to connect to!")
+
+
+
+
+
 NetworkTable.setIPAddress(ipadd)
 NetworkTable.setClientMode()
 NetworkTable.initialize()
 sd = NetworkTable.getTable("SmartDashboard")
+
 class EchoWebSocket(tornado.websocket.WebSocketHandler):
     def __init__(self,application, request, **kwargs):
         sd.addTableListener(self.valueChanged)
         super().__init__(application,request)
-        
-        
+
+
     def changeValue(self,key,value):
             print(key,'has been changed to',value)
-            
+
             message={'key':key,'value':value,'sendTo':"#"+key,'event':'valChanged'}
             sendMessage=json.dumps(message)
             self.write_message(sendMessage,False)
@@ -40,18 +51,21 @@ class EchoWebSocket(tornado.websocket.WebSocketHandler):
             #whose id=key to value
     def valueChanged(self,table, key, value, isNew):
             IOLoop.current().add_callback(self.changeValue,key,value)
-            
-    
+
+
     def check_origin(self, origin):
         return True
+
     def open(self):
         print ("WebSocket opened")
+
     def writeStringToNetworkTable(self,message):
         #message=key|message
         key=message['key']
         newMessage=message["value"]
         print('key-',key,',message-',newMessage)
         sd.putString(key,newMessage)
+
     def getStringValue(self,message):
         key=message['key']
         value=sd.getString(message['key'])
@@ -61,27 +75,30 @@ class EchoWebSocket(tornado.websocket.WebSocketHandler):
         sendmsg=json.dumps(message)
         self.write_message(sendmsg, False)
     def on_message(self, message):
+
         data=json.loads(message)
         #data=json.
         print("Recieved-",data)
         actiontype=data["action"]
+
         if actiontype=='read':
             self.getStringValue(data)
+
         elif actiontype=="write":
             self.writeStringToNetworkTable(data)
+
     def on_close(self):
         print("WebSocket closed")
-        
+
 
 app = tornado.web.Application([
     (r'/ws', EchoWebSocket),
     (r"/(.*)", tornado.web.StaticFileHandler, {"path": os.path.dirname(__file__)}),
-    
+
 ])
+
 if __name__ == '__main__':
     parse_command_line()
     app.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
 #connectToIP(ipadd)
-        
-        
