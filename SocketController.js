646498f9580958577024fda7ef3a6c953189eb75
lleontan <lleontan@gmail.com>
2015-02-03 17:34:37 -0500

    now reads and stores data from localstore in keyStore.

--- SocketController.js
+++ SocketController.js
@@ -1,57 +1,5 @@
-//socketController should handle all of the websocket Code,
-
 var keyStore={
 }
-var testSettingsObject={
-	"names":[1,2,3],
-	"values":["a","b","c"]
-}
-function writeSettings(divName){
-	//writes settings UI elements from file, to UI
-	var div=d3.select(divName);//at the moment, all of the id's should be ".settingsBox"
-
-/*	THIS SHOULD BE CORRECT, PUT BACK IN WHEN IT CAN WRITE DATA
-	var jsonStr=keyStore[document.title+"Vals"];		//get the array of stuff to display, title of document+"Vals"
-	//each entity within titleVals should be a JSON object that have arrays for names and values
-	var parsedData=JSON.parse(jsonStr);//parsedData is our dataset
-*/
-	var jsonStr=JSON.stringify(testSettingsObject);//testing, delete when done	
-	console.log(jsonStr);
-var parsedData=JSON.parse(jsonStr);
-
-	div.selectAll("div")		//selecting all the divs within our div
-	.data(parsedData["names"])
-	.enter()
-	.append("span")
-	.text(function(d,i){
-		return parsedData["names"][i];
-	})
-	.append("form")				//appending submitForms for each data
-	.append("input")			//appending input forms in the submit forms
-	.attr("type","text")
-	.attr("value",function(d,i){			//setting values
-		return parsedData["values"][i];
-	})
-	.attr("id",function(d,i){				//setting id's
-		return parsedData["names"][i];
-	});
-
-}
-
-$(document).ready(function(){
-
-	writeSettings(".settingsBox");
-	
-	var retrievedData=localStorage.getItem("keyStore");
-	var data=JSON.parse(retrievedData);
-	if(data==null){
-		console.log("read from localhost == null");
-	}
-	else{
-	keyStore=data;}
-	console.log(keyStore);
-});
-
 jQuery(function($) {
 	var socket;
 	if (!("WebSocket" in window)) {
@@ -63,8 +11,15 @@ jQuery(function($) {
 		//checks keyStore for val of key
 		return keyStore[key];
 	}
-	function setValue(val){//val is a json
+	function setValue(val){//val is a json string
 		socket.send(val);
+	}
+	function setKeyValue(key,value){//key and value are strings
+		var val={
+			"key":key,
+			"value":value
+		}
+		socket.send(JSON.stringify(val));
 	}
 	function setup() {
 
@@ -84,12 +39,12 @@ jQuery(function($) {
 				var event = data['event'];
 				keyStore[key]=value;
 				console.log("Message Recieved-key"+key+"-"+value);
-							
-				var val=JSON.stringify(keyStore);
-				localStorage.setItem("keyStore", val);
+
+				var val=JSON.stringify(keyStore["Local"]);
+				localStorage.setItem("Local", val);	//write things within keystore, Local to localStoreage
 			}
 			socket.onclose = function() {
-				console.log("socket Closed");
+				console.log("Socket Closed");
 				setTimeout(function(){
 				    setup();
 				}, 300);
