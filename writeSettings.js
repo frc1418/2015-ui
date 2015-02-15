8e523c91edde4682c09eb4a95560f051a38f03cd
lleontan <lleontan@gmail.com>
2015-02-15 17:25:51 -0500

    added auto tuning. is not working do not merge

--- writeSettings.js
+++ writeSettings.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var div;
 function writeSettingsFromLocal(divName,key){
 	//appends saved settings to a html element with name=divName,key is the dataset within local
@@ -19,8 +21,10 @@ function writeSettingsFromLocal(divName,key){
 
 	//D3 Starts Here
 	function setTuningVal(id){
+		console.log('ID IS ',id);
 		d3.event.preventDefault();
 		//var t=$("[id=\'"+id+"\']");
+
 		var docdiv=document.getElementById(id);
 		var t=$(docdiv);
 		var type=t.attr('returnType');
@@ -44,72 +48,77 @@ function writeSettingsFromLocal(divName,key){
 		Socket.setValue(Message,true);
 	}
 function writeSettings(data,divname){		//takes an object with 2 arrays, names and values
-	try{
-		div=d3.select(divname);				//selecting the html element
-		div.selectAll("div")								//selecting all the divs within our div
-		.data(data["names"])
+	div=d3.select(divname);				//selecting the html element
+
+	div.selectAll('div')
+		.data(data['divs'])
+		.enter()
+		.append('div')
+		.attr('id',function(d,i){
+			return 'divId'+i;
+			})
+		.style('float','left');
+
+
+		for(var a=0;a<data['divs'].length;a++){
+		var eachDiv=d3.select('#divId'+a);
+		eachDiv.selectAll("div")								//selecting all the divs within our div
+		.data(data["divs"][a]['names'])
 		.enter()
 		.append("span")
 		.text(function(d,i){
-			return data["names"][i];
+			console.log('txtCalled');
+			var str=data["divs"][a]['names'][i];
+			var indexOf=str.indexOf('SmartDashboard')+16;
+			return str.substring(indexOf);
 		})
 		.append("form")														//appending submitForms for each data
-		/*.attr("id",function(d,i){									//setting id's of each form
-			return data["names"][i];
-		})*/
 		.on("submit",function(d,i){
-			var id=data["names"][i];
-			var val=data["values"][i];
-			var func=setTuningVal(id);
+				console.log('asdfasdfasdf');
+
+			var theid=data["divs"][a]['names'][i];
+			var val=data["divs"][a]['values'][i];
+			var func=setTuningVal(theid);
 			return func;
 		})
 		.append("input")										//appending input forms in the submit forms
 		.attr("type","text")
 		.attr('returnType',function(d,i){
-			return typeof(data['values'][i]);
+			console.log('returnTypeCalled');
+			return typeof(data['divs'][a]['values'][i]);
 		})
 		.attr("value",function(d,i){						//setting values
-			return data["values"][i];
+			return data["divs"][a]['values'][i];
 		})
 		.attr("id",function(d,i){									//setting id's of each form
-		return data["names"][i];
+		return data["divs"][a]['names'][i];
 	});
-
-	}
-	catch(err){
-
-		console.log(err.message);
-	}
+		}
 }
+
 function writeArray(IdArray,divname){
 	//use an array of values to write tuning variables
 	//idArray is a array of delimiters to display
 	var end=IdArray.length;
 
 		var data={};
-		data.names=new Array();
-		data.values=new Array();
-		console.log(end);
+		data.divs=new Array();
 	for(var a=0;a<end;a++){
 
+		var Div={
+			'names':new Array(),
+			'values':new Array()
+		};
+		data['divs'].push(Div);
 		for (var property in keyStore){//for every key in keyStore
-			//console.log(property," is checked");
 			if (keyStore.hasOwnProperty(property)) {
 					var contains=property.indexOf(IdArray[a]);
-					//console.log(str," is being compared to ", data.names[a]);
-						if(contains!=-1){
-									data["names"].push(property);
-									data["values"].push(keyStore[property]);
-						}
+					if(contains!=-1){
+									data['divs'][a]['names'].push(property);
+									data['divs'][a]['values'].push(keyStore[property]);
 					}
-				}
+			}
+		}
 	}
 	writeSettings(data,divname);
 }
-
-$(document).ready(function(){											//on startup write settings
-	//writeSettingsFromLocal(".settingsBox");
-	//write a set of tuning values for every delimiter in the array
-
-
-});
