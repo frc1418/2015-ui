bec5e83ab7a3f91d79987434a0ece6e517ac9099
Dustin Spicuzza <dustin@virtualroadside.com>
2015-02-22 21:52:22 -0500

    Disable file caching

--- driverStationServer.py
+++ driverStationServer.py
@@ -79,6 +79,11 @@ class MyStaticFileHandler(tornado.web.StaticFileHandler):
     # This is broken in tornado, disable it
     def check_etag_header(self):
         return False
+    
+    def set_extra_headers(self, path):
+        # Disable caching
+        self.set_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
+
 
 def main():
 
