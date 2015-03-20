00f07514950de538bf223ddf23033c88b268ff15
lleontan <lleontan@gmail.com>
2015-03-20 10:04:44 -0400

    misc

--- imgStreaming.js
+++ imgStreaming.js
@@ -1,4 +1,3 @@
-
 /* Copyright (C) 2007 Richard Atterer, richard©atterer.net
 	This program is free software; you can redistribute it and/or modify it
 	under the terms of the GNU General Public License, version 2. See the file
