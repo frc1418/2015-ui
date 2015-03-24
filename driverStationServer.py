083e1b2ab53d770fe162ea53c5d66049f43f3292
Dustin Spicuzza <dustin@virtualroadside.com>
2015-03-24 01:18:25 -0400

    More pynetworktables2js integration

--- driverStationServer.py
+++ driverStationServer.py
@@ -1,114 +1,70 @@
 #!/usr/bin/env python3
 
-import tornado.ioloop
-from tornado.ioloop import IOLoop
+from os.path import dirname, join
+
 import tornado.web
-import tornado.websocket
-import sys
-import time
-from networktables import NetworkTable
-from tornado.websocket import WebSocketHandler
-from tornado.websocket import WebSocketClosedError
+from tornado.ioloop import IOLoop
 from tornado.options import define, options, parse_command_line
+
+from networktables import NetworkTable
+from pynetworktables2js import get_handlers, NonCachingStaticFileHandler
+
 import logging
-import json
-import os.path
-from threading import RLock
-logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('dashboard')
 
+# Setup logging
+log_datefmt = "%H:%M:%S"
+log_format = "%(asctime)s:%(msecs)03d %(levelname)-8s: %(name)-20s: %(message)s"
 
+logging.basicConfig(datefmt=log_datefmt,
+                    format=log_format,
+                    level=logging.DEBUG)
 
-class WebSocket(tornado.websocket.WebSocketHandler):
-
-    #
-    # WebSocket API
-    #
-
-    def check_origin(self, origin):
-        return True
+class ConfigHandler(tornado.web.RequestHandler):
+    '''Writes JSON that the HTML page can retrieve'''
     
-    def open(self):
-
-        self.ioloop = IOLoop.current()
-        self.nt = NetworkTable.getGlobalTable()
-        NetworkTable.addGlobalListener(self.valueChanged, immediateNotify=True)
-        self.nt.putValue('imgUrl', options.camUrl)
-
-    def on_message(self, message):
-        data=json.loads(message)
-
-        key = data['key']
-        val = data['value']
-        print('key-',key,' val-',val)
-        print('key-',key,',val-',val,' type is ', type(val))
-
-        self.nt.putValue(key, val)
-   
-    def on_close(self):
-        print("WebSocket closed")
-        NetworkTable.removeGlobalListener(self.valueChanged)
-
-    #
-    # NetworkTables specific stuff
-    #
-            
-    def valueChanged(self, key, value, isNew):
-        self.ioloop.add_callback(self.changeValue, key, value,"valueChanged")
-
-    def changeValue(self, key, value, event):
-        #sends a message to the driverstation
-        message={'key':key,
-                 'value':value,
-                 'event':event}
+    def get(self):
+        self.write({
+            'frontcam': options.frontcam,
+            'backcam': options.backcam
+        })
         
-        try:
-            self.write_message(message, False)
-        except WebSocketClosedError:
-            print("websocket closed when sending message")
+
 
 def init_networktables(ipaddr):
 
-    print("Connecting to networktables at %s" % ipaddr)
+    logger.info("Connecting to networktables at %s" % ipaddr)
     NetworkTable.setIPAddress(ipaddr)
     NetworkTable.setClientMode()
     NetworkTable.initialize()
-    print("Networktables Initialized")
-
-
-class MyStaticFileHandler(tornado.web.StaticFileHandler):
-
-    # This is broken in tornado, disable it
-    def check_etag_header(self):
-        return False
-    
-    def set_extra_headers(self, path):
-        # Disable caching
-        self.set_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
+    logger.info("Networktables Initialized")
 
 
 def main():
-    define("camUrl", default="http://10.14.18.2:8080", help="url of the cam feed", type=str)
+    define("frontcam", default="http://10.14.18.2:5800", help="URL for the front camera", type=str)
+    define("backcam", default="http://10.14.18.2:5801", help="URL for the back camera", type=str)
     define("host", default='127.0.0.1', help="Hostname of robot", type=str)
     define("port", default=8888, help="run on the given port", type=int)
 
     parse_command_line()
+    
+    
 
     init_networktables(options.host)
-
-    app = tornado.web.Application([
-        (r'/ws', WebSocket),
-        (r"/()", MyStaticFileHandler, {"path": os.path.join(os.path.dirname(__file__), 'UI_MainPage.html')}),
-        (r"/(.*)", MyStaticFileHandler, {"path": os.path.dirname(__file__)}),
-    ])
     
-    print("Listening on http://localhost:%s/" % options.port)
-    print("- Websocket API at ws://localhost:%s/ws" % options.port)
+    app = tornado.web.Application(
+        get_handlers() + [
+            (r"/config.json", ConfigHandler),
+            (r"/()", NonCachingStaticFileHandler, {"path": join(dirname(__file__), 'UI_MainPage.html')}),
+            (r"/(.*)", NonCachingStaticFileHandler, {"path": dirname(__file__)}),
+        ]
+    )
+    
+    logger.info("Listening on http://localhost:%s/" % options.port)
 
     app.listen(options.port)
-    tornado.ioloop.IOLoop.instance().start()
+    IOLoop.instance().start()
 
 
 if __name__ == '__main__':
     main()
-
-#connectToIP(ipadd)
