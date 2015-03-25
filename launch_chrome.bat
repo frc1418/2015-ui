ccdcc6ac8b08e98bd8d0e2a47b2b59b85acc18e7
Dustin Spicuzza <dustin@virtualroadside.com>
2015-03-24 23:18:28 -0400

    Fix typo

--- launch_chrome.bat
+++ launch_chrome.bat
@@ -1,4 +1,4 @@
 @echo off
 
 REM The page order is so random...
-start http://localhost:8888/Robot.html http://localhost:8888 http://localhost:8888/Atom.html --window-size=1366,570 --window-position=0,0
+start http://localhost:8888/Robot.html http://localhost:8888 http://localhost:8888/Aton.html --window-size=1366,570 --window-position=0,0
